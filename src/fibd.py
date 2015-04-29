64d021207cbe1737001aeb1a4f123aa9eed7b50b
Matt Garner <matt.j.garner@gmail.com>
2015-04-29 15:39:47 +0100

    Started fibd

--- src/fibd.py
+++ src/fibd.py
@@ -3,11 +3,19 @@ Created on 19 Mar 2015
 
 @author: matt
 
-Given: Positive integers n≤100 and m≤20.
+Given: Positive integers n <= 100 and m <= 20.
 
 Return: The total number of pairs of rabbits that will remain after the n-th month
 if all rabbits live for m months.
 '''
+import __main__
 
-def fibd():
-    
+def fibd(input_filepath):
+    input_file = open(input_filepath, "r")
+    input = input_file.readline()
+    n, m = input.split()
+    print "m %d" % m
+    print "n %d" % n
+
+if __name__ == __main__:
+    fibd()
